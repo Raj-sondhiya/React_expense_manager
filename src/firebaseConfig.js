ef13bea751f21bf61e3a2e610541e9936884f70b
Ramraj Panwar <113770711+Raj-sondhiya@users.noreply.github.com>
2023-10-02 16:01:21 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -5,17 +5,10 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import "firebase/compat/database";
 const firebaseConfig = {
-    apiKey: "AIzaSyBj05u0MvlE5yMXWauga92DmBV1qeMjn-A",
-    authDomain: "expense-manager-99cc0.firebaseapp.com",
-    databaseURL: "https://expense-manager-99cc0-default-rtdb.firebaseio.com",
-    projectId: "expense-manager-99cc0",
-    storageBucket: "expense-manager-99cc0.appspot.com",
-    messagingSenderId: "121696770914",
-    appId: "1:121696770914:web:e394224381d67241acea3b"
+    // firebase Configuration code
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 
-export { app, database }; // Export the Firebase database instance
+export { app, database }; 
